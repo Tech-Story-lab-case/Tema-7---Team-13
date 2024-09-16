7f5e014ee191f6a16cf5e454e0441c69a8a43d5d
juliewbech <jube0001@stud.kea.dk>
2024-09-16 15:56:34 +0200

    js added

---
+++ javascript.js
@@ -0,0 +1,35 @@
+const key =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jc2tzeHdland4c3ltcXlha3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5MTM3NDgsImV4cCI6MjA0MTQ4OTc0OH0.gfO6uEwbi_JfAoGmaWYRt-o7OHdq_ftJdNs6eNGPBOw";
+
+const args = {
+  headers: {
+    apikey: key,
+  },
+};
+
+fetch(
+  "https://mcsksxwejwxsymqyakzx.supabase.co/rest/v1/techandstorylab?category=eq.Studio facilities&select=id,navn,category",
+  args
+)
+  .then((response) => response.json())
+  .then(showItems);
+
+function showItems(items) {
+  items.forEach(showSingleItem);
+}
+
+function showSingleItem(item) {
+  console.log("item", item);
+  /*
+  //fanger template
+  const template = document.querySelector("template").content;
+
+  //cloner
+  const clone = template.cloneNode(true);
+  //ændrer indhold
+  clone.querySelector("a").textContent = cat.category;
+  clone.querySelector("a").href = `productlist.html?category=${cat.category}`;
+  //appender
+  document.querySelector(".categorylist").appendChild(clone);
+  */
+}
