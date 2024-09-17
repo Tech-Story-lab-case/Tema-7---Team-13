b2ebf93698db05fd513994b253959fd42b7c3552
juliewbech <jube0001@stud.kea.dk>
2024-09-17 09:39:37 +0200

    jonas' input

--- productlist.js
+++ productlist.js
@@ -1,56 +1,21 @@
-window.addEventListener("DOMContentLoaded", init);
+fetch("https://mcsksxwejwxsymqyakzx.supabase.co/rest/v1/techandstorylab?navn=")
+  .then((response) => response.json())
+  .then((data) => showProducts(data));
 
-const URLparams = new URLSearchParams(window.location.search);
-const category = URLparams.get("category");
-
-const productURI =
-  "https://mcsksxwejwxsymqyakzx.supabase.co/rest/v1/techandstorylab?category=" +
-  category;
-
-let productList;
-let productTemplate;
-
-function init() {
-  productList = document.querySelector("main");
-  console.log("productList", productList);
-
-  productTemplate = document.querySelector("#smallProductTemplate").content;
-  console.log("#smallProductTemplate", productTemplate);
-
-  fetch(productURI)
-    .then((response) => {
-      //console.log("response", response);
-      return response.json();
-    })
-
-    .then(logJSON);
+function showProducts(products) {
+  products.forEach(showProduct);
 }
 
-function logJSON(json) {
-  json.forEach(showProducts);
-}
+function showProduct(product) {
+  console.log(product);
+  //fang template
+  const template = document.querySelector("#smallProductTemplate").textContent;
+  //lav kopi
+  const copy = template.cloneNode(true);
 
-function showProducts(product) {
-  const clone = productTemplate.cloneNode(true);
-  clone.querySelector("h3").textContent = product.productdisplayname;
-  console.log(clone.querySelector(".price"));
-  clone.querySelector(".subtle").textContent = product.brandname;
-  clone.querySelector(".price").textContent = product.price;
-  clone.querySelector(
-    "img"
-  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
-  clone.querySelector("img").alt = product.productdisplayname;
-  clone.querySelector("a").href = `product.html?id=${product.id}`;
-  if (product.soldout) {
-    clone.querySelector("article").classList.add("soldOut");
-  }
-  if (product.discount) {
-    clone.querySelector("article").classList.add("onSale");
-    clone.querySelector(".discounted p span").textContent = Math.round(
-      product.price - (product.price * product.discount) / 100
-    );
-    clone.querySelector(".discounted p+p span").textContent = product.discount;
-  }
-
-  productList.appendChild(clone);
+  //ændre indhold
+  copy.querySelector(".navn").textContent = product.navn;
+  copy.querySelector(".brand").textContent = product.brand;
+  //appende
+  document.querySelector("main").appendChild(copy);
 }
