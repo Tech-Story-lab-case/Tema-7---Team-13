ae890c2f715944dc357365bdb5ae6f1da2525a56
KarolineMK <kakj0003@stud.kea.dk>
2024-09-18 16:36:15 +0200

    testv3js

--- product.js
+++ product.js
@@ -1,12 +1,7 @@
-// const url = "https://mcsksxwejwxsymqyakzx.supabase.co"
 const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id");
+const id = urlParams.get("id"); 
 
-const url = `https://mcsksxwejwxsymqyakzx.supabase.co/rest/v1/techandstorylab?${id}`
-
-// const url = `https://mcsksxwejwxsymqyakzx.supabase.co/rest/v1/techandstorylab?id=eq.B00007`
-
-
+const url = `https://mcsksxwejwxsymqyakzx.supabase.co/rest/v1/techandstorylab?id=eq.${id}`
 
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jc2tzeHdland4c3ltcXlha3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5MTM3NDgsImV4cCI6MjA0MTQ4OTc0OH0.gfO6uEwbi_JfAoGmaWYRt-o7OHdq_ftJdNs6eNGPBOw"
 
