6535a6c5586796bb96657fcfa21609ae3f0d4fdb
KarolineMK <kakj0003@stud.kea.dk>
2024-09-19 10:00:12 +0200

    lavet orden i image mappen

--- product.js
+++ product.js
@@ -3,7 +3,7 @@ const id = urlParams.get("id");
 
 const url = `https://mcsksxwejwxsymqyakzx.supabase.co/rest/v1/techandstorylab?assetid=eq.${id}`;
 
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jc2tzeHdland4c3ltcXlha3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5MTM3NDgsImV4cCI6MjA0MTQ4OTc0OH0.gfO6uEwbi_JfAoGmaWYRt-o7OHdq_ftJdNs6eNGPBOw";
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jc2tzeHdland4c3ltcXlha3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5MTM3NDgsImV4cCI6MjA0MTQ4OTc0OH0.gfO6uEwbi_JfAoGmaWYRt-o7OHdq_ftJdNs6eNGPBOw`;
 
 
 fetch(url, {
@@ -20,9 +20,6 @@ fetch(url, {
 
 function showProduct(product){
     console.log(product);
-
-    // Opens the array, choose the first object in the array and access the data. 0 = first item in programming language.
-    // const productInfo = product[0];
 
     document.querySelector(".productname").textContent = productInfo.navn;
     document.querySelector(".productBox .brand").textContent = productInfo.brand;
